88648414186966c7098531b6233aa73d79ad9ac2
Khadeja R Anab <kay.are.aa@gmail.com>
2024-04-24 21:58:31 +0500

    added logins and bin

--- app/src/main/java/com/example/assignment3/VaultLogins.java
+++ app/src/main/java/com/example/assignment3/VaultLogins.java
@@ -3,9 +3,7 @@ package com.example.assignment3;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.Toast;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,35 +11,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.util.Objects;
+public class VaultLogins extends AppCompatActivity {
 
-public class Home extends AppCompatActivity {
-
-    LinearLayout llLogin, llBin;
+    ImageButton btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_vault_logins);
         init();
 
         SharedPreferences sPref = getSharedPreferences("Login", MODE_PRIVATE);
         sPref.getBoolean("isLogin", true);
 
-        llLogin.setOnClickListener(view -> {
-            Intent intent = new Intent(Home.this, VaultLogins.class);
-            startActivity(intent);
-            finish();
-        });
-
-        llBin.setOnClickListener(view -> {
-            Intent intent = new Intent(Home.this, VaultBin.class);
+        btnBack.setOnClickListener(view -> {
+            Intent intent = new Intent(VaultLogins.this, Home.class);
             startActivity(intent);
             finish();
         });
     }
     private void init()
     {
-        llLogin = findViewById(R.id.llLogin);
-        llBin = findViewById(R.id.llBin);
+        btnBack = findViewById(R.id.btnBack);
     }
 }
